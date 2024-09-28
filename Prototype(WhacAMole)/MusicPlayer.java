6997487610b338ebafdc508cddd65f8d1ef9fc90
Ozie-QM <ozimelaozimela@gmail.com>
2024-09-28 23:54:48 +0200

    Update MusicPlayer.java
    I've commented most of the code for clarity

--- Prototype(WhacAMole)/MusicPlayer.java
+++ Prototype(WhacAMole)/MusicPlayer.java
@@ -7,23 +7,24 @@ import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
-public class MusicPlayer {
+public class MusicPlayer { // Define a public class named MusicPlayer for playing audio files
+
 
 	// Added a static Clip variable to hold the current clip
 	private static Clip clip;
 	
 	public static void RunMusic(String path) {
 		try {
-			AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File(path));
-			Clip clip = AudioSystem.getClip();
-			clip.open(inputStream);
-			clip.loop(Clip.LOOP_CONTINUOUSLY);
-			clip.start();
-		} catch (UnsupportedAudioFileException e) {
+			AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File(path)); // Create an AudioInputStream object from the specified file path
+			Clip clip = AudioSystem.getClip(); // Get an audio clip from the AudioSystem
+			clip.open(inputStream); // Open the audio clip using the input stream
+			clip.loop(Clip.LOOP_CONTINUOUSLY); // Loop the audio clip indefinitely 
+			clip.start(); 
+		} catch (UnsupportedAudioFileException e) { // Catch UnsupportedAudioFileException if the audio file is unsupported
+			e.printStackTrace(); // Print the error message
+		} catch (IOException e) { // Catch IOException if there's an input/output error
 			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (LineUnavailableException e) {
+		} catch (LineUnavailableException e) { 
 			e.printStackTrace();
 		}
 	}
